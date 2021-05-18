59d4a792d05a502ba00161ab50fda2634bc1f7a2
Travis <travisfinch01@gmail.com>
2021-05-18 11:41:06 -0400

    more

--- scripts/windows-crosscompile/dependencies.sh
+++ scripts/windows-crosscompile/dependencies.sh
@@ -9,10 +9,13 @@ sudo apt-get install -yqq  p7zip-full autoconf automake autopoint bash bison bzi
 sudo apt-get -qq update
 sudo apt --fix-broken install python-pycurl python-apt
 #sudo add-apt-repository -y "deb http://pkg.mxe.cc/repos/apt/debian wheezy main"
-echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mxeapt.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB && sudo apt-get -qq update
-sudo apt-get install -yqq mxe-i686-w64-mingw32.static-boost mxe-i686-w64-mingw32.static-qttools
-
+#echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mxeapt.list
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB && sudo apt-get -qq update
+#sudo apt-get install -yqq mxe-i686-w64-mingw32.static-boost mxe-i686-w64-mingw32.static-qttools
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 86B72ED9
+sudo add-apt-repository 'deb [arch=amd64] http://mirror.mxe.cc/repos/apt trusty main'
+sudo apt-get update
+apt-cache search mxe-
 
 #xenial
 #Missing requirement: libtool
